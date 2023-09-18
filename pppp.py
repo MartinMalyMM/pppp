9a090083e445f25865b87cfb25953c1e8c9ec0fb
Martin Maly <martin.maly.mm@email.cz>
2023-09-18 14:33:37 +0100

    removed word radial, improved setting of paths etc.

--- pppp.py
+++ pppp.py
@@ -8,11 +8,23 @@ import numpy as np
 
 # ----------------------------------------------------------------------
 # EXAMPLE USAGE
+# dials.python pppp.py --dir /dls/x02-1/data/2022/mx15722-39/cheetah/ --files 133451 --geom /path/to/geometry_refinement/refined.expt --geom_crystfel /path/to/geometry1.geom
 # dials.python pppp.py --geom /path/to/geometry_refinement/refined.expt --dir /dls/x02-1/data/2022/mx15722-39/cheetah/ --files 133451-0 133451-1 133451-2
 # ----------------------------------------------------------------------
-
-# assuming Python3 (e.g. dials.python) and qsub on GNU/Linux
-
+#
+# Dependencies: qsub and Python3 (e.g. dials.python) on GNU/Linux
+#
+# After run, results are available in files average_intensity_all.csv and average_intensity_all.png
+#
+#
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#            IMPORTANT SETTING - PATHS TO DIALS AND CRYSTFEL
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+SOURCE_DIALS = "module load dials/nightly"
+# SOURCE_DIALS = "source /opt/dials/dials"
+SOURCE_CRYSTFEL = "module load crystfel"
+# SOURCE_CRYSTFEL = ""
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 # https://stackoverflow.com/questions/2785821/is-there-an-easy-way-in-python-to-wait-until-certain-condition-is-true
 def wait_until_not_empty(filename, period=5):
@@ -46,23 +58,29 @@ def wait_until_qjob_finished(job_id, period=5):
     return False
 
 
-def plot_histogram(inputfile='radial_average_all.csv', outputplot='radial_average_all.png'):
+def plot_histogram(inputfile='average_intensity_all.csv', outputplot='average_intensity_all.png'):
+    print("a")
     df = read_csv(inputfile, header=None, index_col=False, sep=',',
-                  names=("runevent", "radial_average"))
-    x = df["radial_average"].to_numpy()
+                  names=("runevent", "average_intensity"))
+    print("b")
+    x = df["average_intensity"].to_numpy()
+    print("c")
     n_images = x.size
     n, bins, patches = plt.hist(x, 250, density=True, facecolor='g', alpha=0.75)
     plt.xlim([0, 90])
-    plt.xlabel('Radial average')
+    plt.xlabel('Average total scattered intensity')
     plt.grid(True)
+    print("d")
     plt.savefig(outputplot, bbox_inches="tight", dpi=200)
+    print("e")
     plt.title('Number of images used: ' + str(n_images))
+    print("f")
     return outputplot
 
 
 def run():
     parser = argparse.ArgumentParser(
-        description="pppp - Pump and Probe Processing Pipeline - 1st script"
+        description="pppp - Pump and Probe Processing Pipeline - 1st script - calculate average total scattered intensity"
     )
     parser.add_argument(
         "--dir", "--path",
@@ -121,8 +139,9 @@ def run():
         os.mkdir(f)
         os.chdir(f)
         with open("tags.sh", "w") as tags_sh:
-            tags_sh.write(f"""module load dials
-dials.stills_process show_image_tags=true {args.path}/{f}/run{f}.h5 > tags.txt""")
+            tags_sh.write(
+                SOURCE_DIALS + "\n" + \
+                f"""dials.stills_process show_image_tags=true {args.path}/{f}/run{f}.h5 > tags.txt""")
         subprocess.check_call(['chmod', '+x', "tags.sh"], encoding="utf-8")
         print(f"Executing qsub tags.sh for {f}...")
         if not args.sim:
@@ -174,24 +193,25 @@ dials.stills_process show_image_tags=true {args.path}/{f}/run{f}.h5 > tags.txt""
             tags_txt_lines_strip = ""
         with open("tags.txt", "w") as tags_txt:
             tags_txt.writelines(tags_txt_lines_strip)
-        with open("filter_radial_av.sh", "w") as filter_sh:
-            filter_sh.write(r'''module load dials/nightly
-dxtbx.radial_average reference_geometry=''' + args.geom + r" show_plots=false " + args.path + r"/" + f + r"/run" + f + r'''.h5 > filter_radial_av_tmp.log
-cat filter_radial_av_tmp.log | grep Average | awk '{print $5}' > filter_radial_av.log
+        with open("filter_average_intensity.sh", "w") as filter_sh:
+            filter_sh.write(
+                SOURCE_DIALS + "\n" + \
+                r'''dxtbx.radial_average reference_geometry=''' + args.geom + r" show_plots=false " + args.path + r"/" + f + r"/run" + f + r'''.h5 > filter_average_intensity_tmp.log
+cat filter_average_intensity_tmp.log | grep Average | awk '{print $5}' > filter_average_intensity.log
 
 i=1
 while read p; do
   event=$(echo "$p" | cut -c13-18)
   run=$(echo "$p" | cut -c1-12)
-  radav=$(sed "${i}q;d" filter_radial_av.log)
-  printf "%s%s %2s\n" $run$event,$radav >> radial_average.csv
+  radav=$(sed "${i}q;d" filter_average_intensity.log)
+  printf "%s%s %2s\n" $run$event,$radav >> average_intensity.csv
   i=$((i+1))
 done <tags.txt''')
-        subprocess.check_call(['chmod', '+x', "filter_radial_av.sh"], encoding="utf-8")
-        print(f"Executing qsub filter_radial_av.sh for {f}...")
+        subprocess.check_call(['chmod', '+x', "filter_average_intensity.sh"], encoding="utf-8")
+        print(f"Executing qsub filter_average_intensity.sh for {f}...")
         if not args.sim:
             p = subprocess.Popen(
-                ['qsub', '-pe', 'smp', '20', 'filter_radial_av.sh'],# stdin=subprocess.PIPE,
+                ['qsub', '-pe', 'smp', '20', 'filter_average_intensity.sh'],# stdin=subprocess.PIPE,
                 stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                 encoding="utf-8")  # shell=settings["sh"])
             output, err = p.communicate()
@@ -209,23 +229,23 @@ done <tags.txt''')
     print(str(job_ids2))
     print("Now you can have a break - time for tea or coffee!")
 
-    radial_average_merge = []
+    average_intensity_merge = []
     for i, f in enumerate(files):
         os.chdir(f)
         if args.sim:
-            subprocess.check_call(['touch', 'radial_average.csv'], encoding="utf-8")
+            subprocess.check_call(['touch', 'average_intensity.csv'], encoding="utf-8")
             #subprocess.check_call(['touch', 'pump.txt'])
             #subprocess.check_call(['touch', 'probe.txt'])
         else:
             wait_until_qjob_finished(job_ids2[i])
-            with open("radial_average.csv", "r") as f:
+            with open("average_intensity.csv", "r") as f:
                 lines = f.readlines()
-            radial_average_merge = radial_average_merge + lines
+            average_intensity_merge = average_intensity_merge + lines
         os.chdir("..")
 
-    with open("radial_average_all.csv", "w") as f:
-        f.write('\n'.join(radial_average_merge))
-    print("Check data in the file radial_average_all.csv")
+    with open("average_intensity_all.csv", "w") as f:
+        f.write('\n'.join(average_intensity_merge))
+    print("Check data in the file average_intensity_all.csv")
     print("You can use it to plot a histogram.")
 
     try:
@@ -233,7 +253,7 @@ done <tags.txt''')
         import matplotlib.pyplot as plt
         from pandas import read_csv
         print("Plotting the result...")
-        outputplot = plot_histogram(inputfile='radial_average_all.csv', outputplot='radial_average_all.png')
+        outputplot = plot_histogram(inputfile='average_intensity_all.csv', outputplot='average_intensity_all.png')
         print(f"Histogram plotted to {outputplot}")
     except:
         print(f"Histogram not plotted. :-(")
@@ -242,9 +262,9 @@ done <tags.txt''')
         print("Creating events.lst for CrystFEL")
         try:
             p = subprocess.Popen(
-                ['module', 'load', 'crystfel', '&&', 'list_events', '-i', 'files.lst', '-o', 'events.lst', '-g', args.geom_crystfel],# stdin=subprocess.PIPE,
-                 stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                 encoding="utf-8")  # shell=settings["sh"])
+                SOURCE_CRYSTFEL + ' && list_events -i files.lst -o events.lst -g '+ args.geom_crystfel,# stdin=subprocess.PIPE,
+                stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                encoding="utf-8", shell=True)
             output, err = p.communicate()
             if output:
                 print(f"STDOUT: {output}")
