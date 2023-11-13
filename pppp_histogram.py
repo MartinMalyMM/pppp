19bd7974f46e8505ceba379311c652e812178cdf
Martin Maly <martin.maly.mm@email.cz>
2023-11-13 21:49:59 +0000

    changes to be consistent with the main scripts, word radial was deleted

--- pppp_histogram.py
+++ pppp_histogram.py
@@ -7,15 +7,15 @@ from pandas import read_csv
 if len(sys.argv) > 1:
     csvfile = sys.argv[1]
 else:
-    csvfile = 'radial_average_all.csv'
+    csvfile = 'average_intensity_all.csv'
 df = read_csv(csvfile, header=None, index_col=False, sep=',',
               names=("runevent", "radial_average"))
-x = df["radial_average"].to_numpy()
+x = df["average_intensity"].to_numpy()
 n, bins, patches = plt.hist(x, 180, density=True, facecolor='g', alpha=0.75)
 plt.xlim([0, 90])
 n_images = x.size
-plt.locator_params(nbins=9, axis='x')
-plt.xlabel('Radial average')
+# plt.locator_params(nbins=9, axis='x')
+plt.xlabel('Average total scattered intensity')
 plt.grid(True)
 plt.title('Number of images used: ' + str(n_images))
-plt.savefig('radial_average_all.png', bbox_inches="tight", dpi=200)
+plt.savefig('average_intensity_all.png', bbox_inches="tight", dpi=200)
